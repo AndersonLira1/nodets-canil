da81efaa481554e904f51f1aea11af2340b1451a
Anderson Ferreira de Lira <voltrom@pop-os.localdomain>
2021-07-08 16:23:00 -0300

    final project 1.0

---
+++ src/helpers/createMenuObject.ts
@@ -0,0 +1,18 @@
+// Função de seleção do MENU
+
+type MenuOptions = '' | 'all' | 'dog' | 'cat' | 'fish'
+
+export const createMenuObject = (activeMenu: MenuOptions) => {
+    let returnObject = {
+        all: false,
+        dog: false,
+        cat: false,
+        fish: false
+    }
+
+    if(activeMenu !== '') {
+        returnObject[activeMenu] = true
+    }
+
+    return returnObject
+}
